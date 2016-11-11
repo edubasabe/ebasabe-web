d0114031be9ebc5450ac1024d194aad68d4e1d1e
Edu <edubasabe1@gmail.com>
2016-11-10 20:39:20 -0500

    hero del header listo incluso para responsive.

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,20 @@
-$(document).on('ready', function () {
-
-
 function tablet() {
-var $iW = $(window).innerWidth();
-  if ($iW < 992) {
-    return true;
-  }
+    var $iW = $(window).width();
+    if ($iW < 426) {
+        return true;
+    }
+    console.log($iW);
 }
-
 
 // ---
 // Cambio de orden de columnas en Responsive
 // ---
-
-
-if ( tablet() )
-{
-   $('.imagen').insertBefore('.texto');
+if (tablet()) {
+    $('.imagen').insertBefore('.texto');
+} else {
+    //  $('.texto').insertAfter('.imagen');
 }
-else
 
-{
-   $('.texto').insertAfter('.imagen');
-}
+$(document).on('ready', function() {
+
 });
